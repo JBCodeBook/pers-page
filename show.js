95c040164c9fca04c434d92a51620458558712ca
John B <jbuckl2020@gmail.com>
2021-08-31 21:46:35 -0400

    changes  to project cards

--- show.js
+++ show.js
@@ -1,9 +1,17 @@
+console.log("in console1");
+
+
 const open = document.getElementById('about');
-const modal_container = document.getElementById('modal_container_about');
+      console.log(open);
+const modal_container = document.getElementById('modal_container');
+      console.log(modal_container);
 const close = document.getElementById('close');
+console.log(close);
 
 open.addEventListener('click', () =>{
+  console.log("test");
   modal_container.classList.add('show');
+  console.log("test");
 });
 
 close.addEventListener('click', () =>{
